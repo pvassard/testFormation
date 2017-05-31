f718e1d5cc541872fadca311b9c280e4dd96225f
Pierre <pvassard@cci-paris-idf.fr>
2017-05-31 13:37:00 +0200

    Premier commit

---
+++ www/calc.php
@@ -0,0 +1,29 @@
+﻿<?php
+class calc {
+ 
+	static function sum($arg1, $arg2) {
+		$res = $arg1 + $arg2;
+		return $res;
+	}
+ 
+  /**
+   *  Super commentaire trololo
+   */
+	static function minus($arg1, $arg2) {
+		$res = $arg1 - $arg2;
+		return $res;
+	}
+ 
+	static function multi($arg1, $arg2) {
+		$res = $arg1 * $arg2;
+		return $res;
+	}
+ 
+	static function divid($arg1, $arg2) {
+		if ($arg2 == 0) return 'erreur';
+		$res = $arg1 / $arg2;
+		return $res;
+	}
+ 
+}
+?>
